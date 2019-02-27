dbd5d56c75e0e2cf9d80bdfbbdb1fe26e9ebf0d7
Alois Beard <alois.beard@gmail.com>
2019-02-27 12:01:36 +0100

    new bookings can be saved with attributed customer and chef

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:new, :create ]
   end
   resources :bookings, except: [:new]
 
