a8c76fb2b0ea1709be3babfd10d6884cd9b6494e
amir yosef <amiryosef99@outlook.com>
2019-02-28 16:31:49 +0100

    new stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 2019_02_27_112553) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +53,6 @@ ActiveRecord::Schema.define(version: 2019_02_27_112553) do
     t.boolean "availability", default: true
     t.integer "price"
     t.string "cuisine"
-
     t.boolean "chef", default: false
     t.string "image1"
     t.string "image2"
